4c1673cd7f2bf013694eb18016df26002f8e1a57
Obtuse <theobtuseautodidact@gmail.com>
2016-01-09 20:00:40 -0700

    add simple search feature

--- app/models/place.rb
+++ app/models/place.rb
@@ -19,4 +19,11 @@ class Place < ActiveRecord::Base
     0
   end
 
+  def self.search(search)
+    if search
+      where(['name LIKE ? OR address LIKE ?', "#{search}", "#{search}"])
+    else
+      all
+    end
+  end
 end
